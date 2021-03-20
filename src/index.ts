799e1cd282c7ce6f2364d7269e92fde0a93da3e4
Masato Ikeda <masato.ikeda@gmail.com>
2021-03-20 15:45:54 +0900

    Hook on paste

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
-import * as _ from 'lodash';
-
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+const uploadImage = (file: File) => {
+  console.log(file)
 }
 
-document.body.appendChild(component());
+const main = () => {
+  const textarea = document.getElementById('editor') as HTMLTextAreaElement
+  textarea.onpaste = (event) => {
+    const item = event.clipboardData.items[0]
+
+    if (item.kind == 'string') {
+      return
+    }
+
+    if (item.type.startsWith('image/')) {
+      uploadImage(item.getAsFile())
+    }
+
+    event.preventDefault
+  }
+}
+
+main()
